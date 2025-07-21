186a6b534583e561c24a8abbc26e43038533a031
manohar3000 <manohargehlot3000@gmail.com>
2025-07-21 09:34:20 +0530

    cleanup the folder

--- frontend/src/components/LaunchScreen.d.ts
+++
@@ -1,5 +0,0 @@
-declare module './LaunchScreen' {
-  import * as React from 'react';
-  const LaunchScreen: React.FC<{ onFinish: () => void }>;
-  export default LaunchScreen;
-} 
--- frontend/src/components/NyayaLogSidebar.d.ts
+++
@@ -1,5 +0,0 @@
-declare module './NyayaLogSidebar' {
-  import * as React from 'react';
-  const NyayaLogSidebar: React.FC<{ onSelectChat?: (id: number) => void }>;
-  export default NyayaLogSidebar;
-} 
